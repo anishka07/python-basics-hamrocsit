d700bec625b64a7e6e171b54458c9ee72bfaa2f5
anishka07 <anishkamukherjee237@gmail.com>
2025-04-01 16:08:21 +0545

    update day 2

--- main.py
+++ main.py
@@ -6,4 +6,3 @@ if __name__ == '__main__':
         os.makedirs(f"Day {i}", exist_ok=True)
         with open(f"Day {i}/README.md", "w") as f:
             f.write(f"# This is the what we learned in Day {i}\n")
-    print("Done")
