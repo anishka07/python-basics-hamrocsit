cc1d5b94199a4af37c8c1d035318b3aa01fa6c0c
steven <steven@stevens.computer>
2025-04-08 15:00:31 +0545

    assignment solution

--- Day 5/odd_or_even.py
+++ Day 5/odd_or_even.py
@@ -1,1 +1,7 @@
 '''WAP to find if a given number is odd or even'''
+
+a= float(input("enter the value  "))
+if a %2 ==0:
+    print("even")
+else:
+    print("odd")
--- Day 5/vote_eligible.py
+++ Day 5/vote_eligible.py
@@ -1,2 +1,10 @@
 '''WAP to check if a user is eligible to vote or not'''
 # Ask for the age, legal age to vote is 18
+
+
+vote_age = float(input('Enter the age '))
+
+if vote_age >= 18:
+    print("You are elligibe for voting")
+else:
+    print("You are not elligible for voting")
