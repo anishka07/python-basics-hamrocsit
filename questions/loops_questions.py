5a7197af367c42c3e37a730a528bc617780b1c6e
anishka07 <anishkamukherjee237@gmail.com>
2025-04-10 14:18:16 +0545

    loops questions

---
+++ questions/loops_questions.py
@@ -0,0 +1,9 @@
+# 1. Write a for loop to print the numbers from 1 to 10.
+
+# 2. Write a while loop to calculate the sum of the first 10 natural numbers.
+
+# 3. Use a for loop to iterate through a list of fruits and print each fruit.
+
+# 4. Write a while loop that keeps asking the user for input until they type "exit".
+
+# 5. Use a for loop to print the multiplication table of 5 (from 1 to 10).
