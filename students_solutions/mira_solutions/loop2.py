2b01d1d7daa84722b20439240f5b87e01155de35
anishka07 <anishkamukherjee237@gmail.com>
2025-04-10 16:24:53 +0545

    checked

--- students_solutions/mira_solutions/loop2.py
+++ students_solutions/mira_solutions/loop2.py
@@ -2,3 +2,12 @@ m=input ("enter your word : ")
 while m!="exit":
     print("enter your word: ")
 print("exit")
+
+# better code 
+
+while True:
+    user_input = input("Enter your word (or 'exit' to quit): ")
+    if user_input.lower() == "exit":
+        break
+    else:
+        print("You entered:", user_input)
