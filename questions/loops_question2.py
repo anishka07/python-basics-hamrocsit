ce41bbfcf3143359e6027d614aa594ea25859a9d
anishka07 <anishkamukherjee237@gmail.com>
2025-04-10 16:12:55 +0545

    loops questions 2

---
+++ questions/loops_question2.py
@@ -0,0 +1,9 @@
+# 1. Write a for loop to print the even numbers from 2 to 20.
+
+# 2. Write a while loop to calculate the factorial of a given number (e.g., 5).
+
+# 3. Use a for loop to iterate through a list of cities and print each city.
+
+# 4. Write a while loop that keeps asking the user for a number until they enter a negative number.
+
+# 5. Use a for loop to print the squares of numbers from 1 to 10.
