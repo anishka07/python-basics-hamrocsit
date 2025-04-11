f36a3dd07ff417ef790860b2a18f77d07e4ffc8e
anishka07 <anishkamukherjee237@gmail.com>
2025-04-11 10:44:51 +0545

    checked updated code

--- students_solutions/bimala_solutions/loop2_q1.py
+++ students_solutions/bimala_solutions/loop2_q1.py
@@ -1,3 +1,9 @@
 # 1. Write a for loop to print the even numbers from 2 to 20.
 for i in range (2, 21, 2):
     print(i)
+
+
+# another way 
+for i in range(2, 21):
+    if i % 2 == 0:
+        print(i)
